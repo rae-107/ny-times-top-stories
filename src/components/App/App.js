d30e9baec1d7954fca3da033791b3abc0521f9c5
Rae Gebhart <raegebhart@gmail.com>
2023-04-19 16:16:44 -0600

    Implement fetch call

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,35 @@
-import './App.css';
+// import './App.css';
+import { getStories } from "../../apiCalls";
+import { useEffect, useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import { Home } from "../Home/Home";
 
-export const App = () => [
-  
-]
+export const App = () => {
+  const [stories, setStories] = useState([]);
+  const [numResults, setNumResults] = useState(0);
+  const [section, setSection] = useState("home");
+  const [error, setError] = useState('')
+
+  const fetchData = async () => {
+    const data = await getStories();
+    console.log(data)
+    if (typeof data === 'string') {
+      setError(data)
+    } else {
+      setStories(data.results);
+      setNumResults(data.num_results);
+      setSection(data.section);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+    console.log(stories, numResults, section);
+  }, []);
+
+  return (
+    <Routes>
+      <Route path="/" element={<Home setSection={setSection} />} />
+    </Routes>
+  );
+};
