a0855b5dc456aa0fd8a58ce827d9ee726fde0322
Rae Gebhart <raegebhart@gmail.com>
2023-04-19 17:40:48 -0600

    Fix page from crashing if article doesn't have an image

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,34 +5,34 @@ export const Header = ({ setSection }) => {
     <nav>
       <h1>The New York Times</h1>
       <h2>Top Stories</h2>
-      <ul>
-        <li><button onClick={() => setSection("arts")}>Arts</button></li>
-        <li><button onClick={() => setSection("automobiles")}>Automobiles</button></li>
-        <li><button onClick={() => setSection("books")}>Books</button></li>
-        <li><button onClick={() => setSection("business")}>Business</button></li>
-        <li><button onClick={() => setSection("fashion")}>Fashion</button></li>
-        <li><button onClick={() => setSection("food")}>Food</button></li>
-        <li><button onClick={() => setSection("health")}>Health</button></li>
-        <li><button onClick={() => setSection("home")}>Home</button></li>
-        <li><button onClick={() => setSection("insider")}>Insider</button></li>
-        <li><button onClick={() => setSection("magazine")}>Magazine</button></li>
-        <li><button onClick={() => setSection("movies")}>Movies</button></li>
-        <li><button onClick={() => setSection("nyregion")}>NY Region</button></li>
-        <li><button onClick={() => setSection("obituaries")}>Obituaries</button></li>
-        <li><button onClick={() => setSection("opinion")}>Opinion</button></li>
-        <li><button onClick={() => setSection("politics")}>Politics</button></li>
-        <li><button onClick={() => setSection("realestate")}>Real Estate</button></li>
-        <li><button onClick={() => setSection("science")}>Science</button></li>
-        <li><button onClick={() => setSection("sports")}>Sports</button></li>
-        <li><button onClick={() => setSection("sundayreview")}>Sunday Review</button></li>
-        <li><button onClick={() => setSection("technology")}>Technology</button></li>
-        <li><button onClick={() => setSection("theater")}>Theater</button></li>
-        <li><button onClick={() => setSection("t-magazine")}>T Magazine</button></li>
-        <li><button onClick={() => setSection("travel")}>Travel</button></li>
-        <li><button onClick={() => setSection("upshot")}>Up Shot</button></li>
-        <li><button onClick={() => setSection("us")}>US</button></li>
-        <li><button onClick={() => setSection("world")}>World</button></li>
-      </ul>
+      <div className="sub-topics">
+        <button onClick={() => setSection("arts")}>Arts</button>
+        <button onClick={() => setSection("automobiles")}>Automobiles</button>
+        <button onClick={() => setSection("books")}>Books</button>
+        <button onClick={() => setSection("business")}>Business</button>
+        <button onClick={() => setSection("fashion")}>Fashion</button>
+        <button onClick={() => setSection("food")}>Food</button>
+        <button onClick={() => setSection("health")}>Health</button>
+        <button onClick={() => setSection("home")}>Home</button>
+        <button onClick={() => setSection("insider")}>Insider</button>
+        <button onClick={() => setSection("magazine")}>Magazine</button>
+        <button onClick={() => setSection("movies")}>Movies</button>
+        <button onClick={() => setSection("nyregion")}>NY Region</button>
+        <button onClick={() => setSection("obituaries")}>Obituaries</button>
+        <button onClick={() => setSection("opinion")}>Opinion</button>
+        <button onClick={() => setSection("politics")}>Politics</button>
+        <button onClick={() => setSection("realestate")}>Real Estate</button>
+        <button onClick={() => setSection("science")}>Science</button>
+        <button onClick={() => setSection("sports")}>Sports</button>
+        <button onClick={() => setSection("sundayreview")}>Sunday Review</button>
+        <button onClick={() => setSection("technology")}>Technology</button>
+        <button onClick={() => setSection("theater")}>Theater</button>
+        <button onClick={() => setSection("t-magazine")}>T Magazine</button>
+        <button onClick={() => setSection("travel")}>Travel</button>
+        <button onClick={() => setSection("upshot")}>Up Shot</button>
+        <button onClick={() => setSection("us")}>US</button>
+        <button onClick={() => setSection("world")}>World</button>
+        </div>
     </nav>
   );
 };
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,16 +1,35 @@
-import { ArticleCard } from "../ArticleCard/ArticleCard"
-import { Header } from "../Header/Header"
-import './Home.css'
+import { ArticleCard } from "../ArticleCard/ArticleCard";
+import { Header } from "../Header/Header";
+import "./Home.css";
 
 export const Home = ({ setSection, stories }) => {
-  const storyCards = stories.map((story) => <ArticleCard img={story.multimedia[2].url} title={story.title} abstract={story.abstract} byline={story.byline} published_date={story.published_date} />)
+  const storyCards = stories.map((story, index) => {
+    console.log(story.title, story.multimedia)
+    if(story.multimedia) {
+      return <ArticleCard
+        key={index}
+        img={story.multimedia[2].url}
+        title={story.title}
+        abstract={story.abstract}
+        byline={story.byline}
+        published_date={story.published_date}
+      />;
+    } else {
+      return <ArticleCard
+      key={index}
+      img={'/assets/no-photos.png'}
+      title={story.title}
+      abstract={story.abstract}
+      byline={story.byline}
+      published_date={story.published_date}
+    />;
+    }
+  });
 
   return (
     <main>
       <Header setSection={setSection} />
-      <section className="articles-container" >
-        {storyCards}
-      </section>
+      <section className="articles-container">{storyCards}</section>
     </main>
-  )
-}
+  );
+};
