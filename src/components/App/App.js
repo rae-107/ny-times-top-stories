ec68e54688e161205f80a6feceeee35ae3ede905
Rae Gebhart <raegebhart@gmail.com>
2023-04-20 15:57:24 -0600

    Delete console.logs and format App

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,15 +1,14 @@
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+import { Article } from "../Article/Article";
 import { getStories } from "../../apiCalls";
 import { useEffect, useState } from "react";
-import { Routes, Route } from "react-router-dom";
 import { Home } from "../Home/Home";
-import { Article } from "../Article/Article"
+import "./App.css";
 
 export const App = () => {
   const [stories, setStories] = useState([]);
   const [numResults, setNumResults] = useState(0);
   const [section, setSection] = useState("home");
-  const [currentArticle, setCurrentArticle] = useState({});
   const [loadingData, setLoadingData] = useState(true);
   // const [error, setError] = useState('')
 
@@ -22,23 +21,44 @@ export const App = () => {
 
   useEffect(() => {
     fetchData(section);
-    console.log(stories, numResults, section);
   }, []);
 
   return (
     <Routes>
-      <Route path="/" element={<Home setCurrentArticle={setCurrentArticle} setSection={setSection} stories={stories} fetchData={fetchData} section={section} />} />
-      <Route path="/:subTopic" element={<Home setCurrentArticle={setCurrentArticle} setSection={setSection} stories={stories} fetchData={fetchData} section={section} />} />
-      <Route path='/:subTopic/:id'  element={
-        <Article 
-          stories={stories} 
-          fetchData={fetchData} 
-          loadingData={loadingData}
-          section={section} 
-          setSection={setSection} 
-        />
-      } />
-
+      <Route
+        path="/"
+        element={
+          <Home
+            setSection={setSection}
+            stories={stories}
+            fetchData={fetchData}
+            section={section}
+          />
+        }
+      />
+      <Route
+        path="/:subTopic"
+        element={
+          <Home
+            setSection={setSection}
+            stories={stories}
+            fetchData={fetchData}
+            section={section}
+          />
+        }
+      />
+      <Route
+        path="/:subTopic/:id"
+        element={
+          <Article
+            stories={stories}
+            fetchData={fetchData}
+            loadingData={loadingData}
+            section={section}
+            setSection={setSection}
+          />
+        }
+      />
     </Routes>
   );
 };
