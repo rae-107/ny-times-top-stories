a55d31c70b4d3e995da1feedeb90210ca6b14052
Rae Gebhart <raegebhart@gmail.com>
2023-04-19 18:14:30 -0600

    Add styling to home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,12 +3,13 @@ import { Header } from "../Header/Header";
 import "./Home.css";
 
 export const Home = ({ setSection, stories }) => {
+  console.log(stories)
   const storyCards = stories.map((story, index) => {
     console.log(story.title, story.multimedia)
     if(story.multimedia) {
       return <ArticleCard
         key={index}
-        img={story.multimedia[2].url}
+        img={story.multimedia[0].url}
         title={story.title}
         abstract={story.abstract}
         byline={story.byline}
