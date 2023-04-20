44f29017ad41d1f3261307d6e3726c75e3fe45de
Rae Gebhart <raegebhart@gmail.com>
2023-04-20 17:17:30 -0600

    Style details page

--- src/components/Article/Article.js
+++ src/components/Article/Article.js
@@ -1,46 +1,55 @@
-import './Article.css'
+import "./Article.css";
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
-import { Header } from '../Header/Header';
+import { Header } from "../Header/Header";
 
-export const Article = ({ stories, fetchData, loadingData, section, setSection }) => {
+export const Article = ({
+  stories,
+  fetchData,
+  loadingData,
+  section,
+  setSection,
+}) => {
   const { subTopic, id } = useParams();
   const [article, setArticle] = useState({});
 
   useEffect(() => {
-    if(subTopic) {
-      fetchData(subTopic)
+    if (subTopic) {
+      fetchData(subTopic);
     } else {
-      fetchData(section)
+      fetchData(section);
     }
-    if(!loadingData) {
+    if (!loadingData) {
       setArticle(stories[id]);
     }
-  }, [loadingData])
+  }, [loadingData]);
 
-  let articleImg 
+  let articleImg;
 
   if (article.multimedia) {
-    articleImg = article.multimedia[0].url
+    articleImg = article.multimedia[0].url;
   } else {
-    articleImg = '/assets/no-photos.png'
+    articleImg = "/assets/no-photos.png";
   }
 
   return (
     <>
       {loadingData && <h1>-- LOADING --</h1>}
-      {!loadingData &&
-        <section className='article-container'>
-          <Header setSection={setSection}  />
-          <img className='large-article-img' src={articleImg} />
-          <h2>{article.title}</h2>
-          <p>{article.section}</p>
-          <p>{article.byline}</p>
-          <p>{article.published_date}</p>
-          <p>{article.abstract}</p>
-          <a href={article.url} >See full article</a>
-        </section>      
-      }
+      {!loadingData && (
+        <section className="article-container">
+          <Header setSection={setSection} />
+          <div className="large-article-container" >
+            <div className="details" >
+              <h2 className="large-article-title"><em>{article.title}</em></h2>
+              <p>{article.abstract}</p>
+            </div>
+            <img className="large-article-img" src={articleImg} />
+            <p>{article.byline}</p>
+            <p>{article.published_date}</p>
+            <a className="article-url" href={article.url}>See Full Article Here</a>
+          </div>
+        </section>
+      )}
     </>
-  )
-}
+  );
+};
